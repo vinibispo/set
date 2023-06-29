e602f8c411e9b72999f901f454b518fa783d8016
vinibispo <vini.bispo015@gmail.com>
2023-06-29 08:04:30 -0300

    fix: fix empty? method only returning true

--- lib/set.rb
+++ lib/set.rb
@@ -1,5 +1,15 @@
 class Set
-  def empty?
-    true
+  def initialize
+    self.is_empty = true
   end
+
+  def empty?
+    is_empty
+  end
+
+  def add(_element)
+    self.is_empty = false
+  end
+
+  private attr_accessor :is_empty
 end
