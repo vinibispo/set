5ea0a7c09d1551fe5b3953bd3b9d29b8c5bae89e
vinibispo <vini.bispo015@gmail.com>
2023-06-29 08:09:28 -0300

    test: fix empty? method only returning true

--- spec/set_spec.rb
+++ spec/set_spec.rb
@@ -4,7 +4,16 @@ RSpec.describe Set do
   describe '#empty?' do
     it 'returns true for an empty set' do
       set = Set.new
+
       expect(set.empty?).to eq(true)
+    end
+
+    it 'returns false for a non-empty set' do
+      set = Set.new
+
+      set.add('1')
+
+      expect(set.empty?).to eq(false)
     end
   end
 end
